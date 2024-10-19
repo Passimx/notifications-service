5b32e04b6069b9d932518e88be00009c79822888
ramzini22 <ramil.zinnatullin.02@mail.ru>
2024-10-19 15:43:01 +0300

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
-import { version } from '../package.json';
 import { AppModule } from './modules/app.module';
 import { Envs } from './common/envs/envs';
-import { logger } from './common/logger/logger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule);
 
     await app.listen(Envs.main.appPort, Envs.main.host);
-
-    const url = await app.getUrl();
-
-    logger.info(`Server is running on url: ${url} at ${Date()}. Version: '${version}'.`);
 }
 
 bootstrap();
