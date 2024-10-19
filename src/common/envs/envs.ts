41ff8a7a9cd7afede323d0bfa5f41119015f695c
ramzini22 <ramil.zinnatullin.02@mail.ru>
2024-10-19 15:10:34 +0300

    init commit

---
+++ src/common/envs/envs.ts
@@ -0,0 +1,26 @@
+import process from 'process';
+import { config } from 'dotenv';
+import { NumbersUtils } from '../utils/numbers.utils';
+import { BooleanUtils } from '../utils/boolean.utils';
+
+config();
+export const Envs = {
+    main: {
+        host: process.env.APP_HOST || '0.0.0.0',
+        appPort: NumbersUtils.toNumberOrDefault(process.env.APP_PORT, 3000),
+    },
+
+    swagger: {
+        path: process.env.SWAGGER_PATH || 'docs',
+        isWriteConfig: BooleanUtils.strToBoolWithDefault(process.env.SWAGGER_IS_WRITE_CONFIG, false),
+        url: `http://localhost:${process.env.APP_PORT ?? 3000}`,
+        description: 'development',
+    },
+
+    kafka: {
+        host: process.env.KAFKA_HOST,
+        port: process.env.KAFKA_PORT,
+        user: String(process.env.KAFKA_USER),
+        password: String(process.env.KAFKA_PASSWORD),
+    },
+};
