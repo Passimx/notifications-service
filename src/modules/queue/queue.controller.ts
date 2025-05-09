2c45051e0cb42be92814286e5e6fec1ff928c1d1
Tit <tit.4mir@gmail.com>
2025-05-09 19:12:37 +0300

    fixed: fixed comment

--- src/modules/queue/queue.controller.ts
+++ src/modules/queue/queue.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Inject, UseFilters } from '@nestjs/common';
-import { EventPattern, RpcException } from '@nestjs/microservices';
+import { EventPattern } from '@nestjs/microservices';
 import { WsServer } from '../socket/raw/socket-server';
 import { ApiMessageResponseDecorator } from '../../common/decorators/api-message-response.decorator';
 import { KafkaExceptionFilter } from '../exceptionFilters/RpcExceptionFilter';
@@ -13,9 +13,6 @@ export class QueueController {
     @EventPattern('emit')
     @ApiMessageResponseDecorator()
     emit(body: MessageDto) {
-        if (!body) {
-            throw new RpcException('Body is required');
-        }
         this.wsServer.to(body.to).emit(body.event, body.data);
     }
 
